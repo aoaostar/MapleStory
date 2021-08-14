21d9f6d85106c0e57457b4207991aef22d9996ef
aoaostar <i@aoaostar.com>
2021-08-14 10:41:48 -0400

    修复jdk缺失问题

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ echo "
 |                   冒险岛079 FOR CentOS/Ubuntu/Debian
 +----------------------------------------------------------------------
 "
-./jdk_linux/jre/bin/java -cp ./bin/maple.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
+./jdk/jre/bin/java -cp ./bin/maple.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
